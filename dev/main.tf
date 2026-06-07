63854cab9e88b5c0be7e8cf9c2c7b531ba41f4c0
Prabakaran <prabakaran@opsera.io>
2026-06-07 09:57:22 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -13,7 +13,7 @@ output "environment" {
 }
 
 output "message" {
-  value = "Dev pipeline triggered successfully"
+  value = "Dev pipeline triggered successfully pr writeback"
 }
 
 output "module_output" {
