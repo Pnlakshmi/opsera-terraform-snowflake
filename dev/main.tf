9439d4794b3926b9f7ac2eca52a19378f6a5e95a
Prabakaran <prabakaran@opsera.io>
2026-06-18 23:11:10 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -13,7 +13,7 @@ output "environment" {
 }
 
 output "message" {
-  value = "Dev pipeline triggered successfully"
+  value = "Dev pipeline triggered successfully webhook pull and push"
 }
 
 output "module_output" {
